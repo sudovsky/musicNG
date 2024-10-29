757c4a7dcbe833467807fd5fff40651fd6f2f438
Max <sudovsky@cloudunion.ru>
2024-10-29 17:24:52 +0300

    Initial Commit

---
+++ musicNG/musicNGApp.swift
@@ -0,0 +1,17 @@
+//
+//  musicNGApp.swift
+//  musicNG
+//
+//  Created by Max Sudovsky on 29.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct musicNGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ musicNGTests/musicNGTests.swift
@@ -0,0 +1,17 @@
+//
+//  musicNGTests.swift
+//  musicNGTests
+//
+//  Created by Max Sudovsky on 29.10.2024.
+//
+
+import Testing
+@testable import musicNG
+
+struct musicNGTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
