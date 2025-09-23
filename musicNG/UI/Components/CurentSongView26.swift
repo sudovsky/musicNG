751e9d8b0084005d23bdef3974067d55a3b9ff5d
Max <sudovsky@cloudunion.ru>
2025-09-23 11:59:45 +0300

    Music control view rework

---
+++ musicNG/UI/Components/CurentSongView26.swift
@@ -0,0 +1,18 @@
+//
+//  CurentSongView26.swift
+//  musicNG
+//
+//  Created by Max Sudovsky on 16.09.2025.
+//
+
+import SwiftUI
+
+struct CurentSongView26: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CurentSongView26()
+}
--- musicNG/UI/Views/MusicControlView.swift
+++ musicNG/UI/Views/MusicControlView.swift
@@ -25,125 +25,91 @@ struct MusicControlView: View {
         if isVertical == false {
             HStack(spacing: 0) {
                 VinylView()
-//                    .animation(.easeInOut(duration: 0.2), value: imageOffset)
                     .scaleEffect(imageScale)
                     .offset(imageOffset)
                     .opacity(imageOpacity)
-                verticalPart()
-                    .frame(maxWidth: orientationCoordinator.size.width / 2)
+                    .vinilGesture(canSwitchSong: false) {
+                        dismiss()
+                    }
+
+                Spacer()
+                
+                VStack(spacing: 0) {
+                    topPart()
+                    
+                    Spacer()
+                    
+                    verticalPart()
+                        .frame(maxWidth: orientationCoordinator.size.width / 2)
+                }
             }
-            .gesture(
-                DragGesture(minimumDistance: 20)
-                    .onEnded({ val in
-                        let distX = val.location.x - val.startLocation.x
-                        let disty = val.location.y - val.startLocation.y
-                        
-                        if abs(distX) < abs(disty), disty > 0 {
-                            dismiss()
-                        }
-                    })
-            )
             .orientationChange { size, vertical in
                 isVertical = vertical
             }
-            .opacity(isVertical == nil ? 0 : 1)
         } else {
-            verticalPart()
-                .orientationChange { size, vertical in
-                    if isVertical != vertical {
-                        if isVertical == nil {
-                            withAnimation {
-                                isVertical = vertical
-                            }
-                        } else {
-                            isVertical = vertical
-                        }
-                    }
-                }
-                .opacity(isVertical == nil ? 0 : 1)
-        }
-    }
-    
-    func verticalPart() -> some View {
-        VStack(spacing: 0) {
-            HStack(spacing: 0) {
-                Image(systemName: "xmark")
-                    .titleButtonImage(.leading)
-                    .hidden()
+            VStack(spacing: 0) {
+                topPart()
                 
-                Text(MediaPlayer.shared.currentPlaylistName ?? "")
-                    .font(trackFont)
-                    .multilineTextAlignment(.center)
-                    .frame(maxWidth: .infinity, alignment: .center)
-                    .padding(.vertical, 8)
-                
-                Button {
-                    dismiss()
-                } label: {
-                    Image(systemName: "xmark")
-                        .titleButtonImage(.trailing)
-                }
-                .frame(width: 44, alignment: .center)
-            }
-            .padding(.vertical, isVertical == false ? 16 : 0)
-            
-            if isVertical ?? false {
                 Spacer()
-                
+
                 VinylView()
                     .scaleEffect(imageScale)
                     .offset(imageOffset)
                     .opacity(imageOpacity)
-                    .gesture(
-                        DragGesture(minimumDistance: 20)
-                            .onChanged({ val in
-                                let distX = val.location.x - val.startLocation.x
-                                let disty = val.location.y - val.startLocation.y
-                                
-                                if abs(distX) > abs(disty) {
-                                    print(distX)
-                                    withAnimation(.easeInOut(duration: 0.2)) {
-                                        imageOffset = .init(width: distX, height: 0)
-                                        imageScale = getScale(x: distX)
-                                        imageOpacity = getOpacity(x: distX)
-                                    }
-                                }
-                            })
-                            .onEnded({ val in
-                                let distX = val.location.x - val.startLocation.x
-                                let disty = val.location.y - val.startLocation.y
-                                
-                                if abs(distX) < abs(disty), disty > 0 {
-                                    dismiss()
-                                } else if abs(distX) > abs(disty) {
-                                    if abs(distX) < 100 {
-                                        withAnimation(.easeInOut(duration: 0.2)) {
-                                            imageOffset = .zero
-                                            imageScale = 0.85
-                                            imageOpacity = 1
-                                        }
-                                    } else {
-                                        if distX > 0 {
-                                            prev()
-                                        } else {
-                                            next()
-                                        }
-                                    }
-                                }
-                            })
-                    )
+                    .vinilGesture(imageOffset: $imageOffset, imageScale: $imageScale, imageOpacity: $imageOpacity, next: next, prev: prev) {
+                        dismiss()
+                    }
 
                 Spacer()
-            } else {
-                Spacer()
+
+                verticalPart()
+                    .orientationChange { size, vertical in
+                        if isVertical != vertical {
+                            if isVertical == nil {
+                                withAnimation {
+                                    isVertical = vertical
+                                }
+                            } else {
+                                isVertical = vertical
+                            }
+                        }
+                    }
+                    .opacity(isVertical == nil ? 0 : 1)
             }
-
+        }
+    }
+    
+    func topPart() -> some View {
+        HStack(spacing: 0) {
+            Image(systemName: "xmark")
+                .titleButtonImage(.leading)
+                .hidden()
+            
+            Text(MediaPlayer.shared.currentPlaylistName ?? "")
+                .font(trackFont)
+                .multilineTextAlignment(.center)
+                .frame(maxWidth: .infinity, alignment: .center)
+                .padding(.vertical, 8)
+            
+            Button {
+                dismiss()
+            } label: {
+                Image(systemName: "xmark")
+                    .titleButtonImage(.trailing)
+            }
+            .frame(width: 44, alignment: .center)
+        }
+        .padding(.vertical, isVertical == false ? 16 : 0)
+    }
+    
+    func verticalPart() -> some View {
+        VStack(spacing: 0) {
             Text((variables.currentSong?.title ?? variables.currentSong?.name) ?? "")
                 .lineLimit(2)
                 .minimumScaleFactor(0.3)
                 .multilineTextAlignment(.center)
                 .font(mcTrackFont)
-                .frame(maxHeight: 42)
+                .frame(minHeight: 22, maxHeight: 42)
                 .padding(.horizontal, 16)
                 .padding(.top, 24)
             
@@ -207,22 +173,6 @@ struct MusicControlView: View {
         }
     }
     
-    func getScale(x: CGFloat) -> CGFloat {
-        
-        let percent = (abs(x) / (UIScreen.getSize().width)) * 100
-        let result =  0.85 - (0.85 * percent / 100)
-        
-        return result > 0.75 ? result : 0.75
-    }
-
-    func getOpacity(x: CGFloat) -> CGFloat {
-        
-        let percent = (abs(x) / (UIScreen.getSize().width)) * 100
-        let result =  1 - (percent / 100)
-        
-        return result > 0.35 ? result : 0.35
-    }
-    
     func prev() {
         withAnimation(.easeInOut(duration: 0.2)) {
             imageScale = 0.75
@@ -265,6 +215,83 @@ struct MusicControlView: View {
         }
     }
 
+}
+
+struct VinilGesture: ViewModifier {
+
+    var canSwitchSong: Bool = true
+    
+    @Binding var imageOffset: CGSize
+    @Binding var imageScale: CGFloat
+    @Binding var imageOpacity: CGFloat
+    
+    var onClose: () -> Void
+    var next: () -> Void
+    var prev: () -> Void
+
+    func body(content: Content) -> some View {
+        content
+            .gesture(
+                DragGesture(minimumDistance: 20)
+                    .onChanged({ val in
+                        let distX = val.location.x - val.startLocation.x
+                        let disty = val.location.y - val.startLocation.y
+                        
+                        if canSwitchSong, abs(distX) > abs(disty) {
+                            withAnimation(.easeInOut(duration: 0.2)) {
+                                imageOffset = .init(width: distX, height: 0)
+                                imageScale = getScale(x: distX)
+                                imageOpacity = getOpacity(x: distX)
+                            }
+                        }
+                    })
+                    .onEnded({ val in
+                        let distX = val.location.x - val.startLocation.x
+                        let disty = val.location.y - val.startLocation.y
+                        
+                        if abs(distX) < abs(disty), disty > 0 {
+                            onClose()
+                        } else if canSwitchSong, abs(distX) > abs(disty) {
+                            if abs(distX) < 100 {
+                                withAnimation(.easeInOut(duration: 0.2)) {
+                                    imageOffset = .zero
+                                    imageScale = 0.85
+                                    imageOpacity = 1
+                                }
+                            } else {
+                                if distX > 0 {
+                                    prev()
+                                } else {
+                                    next()
+                                }
+                            }
+                        }
+                    })
+            )
+    }
+    
+    func getScale(x: CGFloat) -> CGFloat {
+        
+        let percent = (abs(x) / (UIScreen.getSize().width)) * 100
+        let result =  0.85 - (0.85 * percent / 100)
+        
+        return result > 0.75 ? result : 0.75
+    }
+
+    func getOpacity(x: CGFloat) -> CGFloat {
+        
+        let percent = (abs(x) / (UIScreen.getSize().width)) * 100
+        let result =  1 - (percent / 100)
+        
+        return result > 0.35 ? result : 0.35
+    }
+
+}
+
+extension View {
+    func vinilGesture(canSwitchSong: Bool = true, imageOffset: Binding<CGSize> = .constant(CGSize.zero), imageScale: Binding<CGFloat> = .constant(0), imageOpacity: Binding<CGFloat> = .constant(1), next: @escaping () -> Void = {}, prev: @escaping () -> Void = {}, onClose: @escaping () -> Void) -> some View {
+        modifier(VinilGesture(canSwitchSong: canSwitchSong, imageOffset: imageOffset, imageScale: imageScale, imageOpacity: imageOpacity, onClose: onClose, next: next, prev: prev))
+    }
 }
 
 #Preview {
